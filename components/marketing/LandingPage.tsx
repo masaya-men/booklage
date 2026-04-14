e36b8a3ff5f4f6b25ba00e8b73c154fad607f707
Masaya <masaya@example.com>
2026-04-14 20:10:12 +0900

    feat(lp): add SaveDemoSection with scroll-triggered 3-step flow

--- components/marketing/LandingPage.tsx
+++ components/marketing/LandingPage.tsx
@@ -4,6 +4,8 @@
 import { useSmoothScroll } from '@/lib/scroll/use-smooth-scroll'
 import { useScrollTrigger } from '@/lib/scroll/use-scroll-trigger'
 import { HeroSection } from './sections/HeroSection'
+import { SaveDemoSection } from './sections/SaveDemoSection'
+import { CollageDemoSection } from './sections/CollageDemoSection'
 import styles from './LandingPage.module.css'
 
 export function LandingPage(): React.ReactElement {
@@ -14,6 +16,8 @@ export function LandingPage(): React.ReactElement {
     <div className={styles.wrapper}>
       <div className={styles.content}>
         <HeroSection />
+        <SaveDemoSection />
+        <CollageDemoSection />
       </div>
     </div>
   )
